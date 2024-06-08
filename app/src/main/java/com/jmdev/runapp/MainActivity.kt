04dd24591491e67fba55592a583bc0ba5eed2d79
Jonas Moreira <jonas.andrade7@gmail.com>
2024-06-07 21:59:35 -0300

    Create theme in design system.

--- app/src/main/java/com/jmdev/runapp/MainActivity.kt
+++ app/src/main/java/com/jmdev/runapp/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.jmdev.runapp.ui.theme.RunAppTheme
+import com.jmdev.core.presentation.designsystem.RunAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ core/presentation/designsystem/src/main/java/com/jmdev/core/presentation/designsystem/Color.kt
@@ -0,0 +1,15 @@
+package com.jmdev.core.presentation.designsystem
+
+import androidx.compose.ui.graphics.Color
+
+val RunAppGreen = Color(0xFF00F15E)
+val RunAppGreen30 = Color(0x4D00F15E)
+val RunAppGreen10 = Color(0x1A00F15E)
+val RunAppGreen5 = Color(0x0D00F15E)
+val RunAppBlack = Color(0xFF080707)
+val RunAppGray = Color(0xFF87938C)
+val RunAppGray40 = Color(0x6687938C)
+val RunAppDarkGray = Color(0xFF232624)
+val RunAppWhite = Color(0xFFFAFAFA)
+val RunAppDarkRed = Color(0xFFBB3D3D)
+val RunAppDarkRed5 = Color(0x0DBB3D3D)

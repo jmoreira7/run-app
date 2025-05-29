408eaba8faeb8066f3794d9163766d24da3079b7
jmoreira7 <jonas.andrade7@gmail.com>
2025-05-29 22:31:30 +0200

    Create login clickable text

--- auth/presentation/src/main/java/com/jmdev/auth/presentation/register/RegisterScreen.kt
+++ auth/presentation/src/main/java/com/jmdev/auth/presentation/register/RegisterScreen.kt
@@ -6,15 +6,22 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jmdev.auth.presentation.R
+import com.jmdev.core.presentation.designsystem.Poppins
+import com.jmdev.core.presentation.designsystem.RunAppGray
 import com.jmdev.core.presentation.designsystem.RunAppTheme
 import com.jmdev.core.presentation.designsystem.components.GradientBackground
 import org.koin.androidx.compose.koinViewModel
@@ -48,6 +55,41 @@ fun RegisterScreen(
             Text(
                 text = stringResource(id = R.string.create_account),
                 style = MaterialTheme.typography.headlineMedium
+            )
+            val annotatedString = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        fontFamily = Poppins,
+                        color = RunAppGray
+                    )
+                ) {
+                    append(stringResource(id = R.string.already_have_an_account) + " ")
+                    pushStringAnnotation(
+                        tag = "clickable_text",
+                        annotation = stringResource(id = R.string.login)
+                    )
+                    withStyle(
+                        style = SpanStyle(
+                            fontWeight = FontWeight.SemiBold,
+                            color = MaterialTheme.colorScheme.primary,
+                            fontFamily = Poppins
+                        )
+                    ) {
+                        append(stringResource(id = R.string.login))
+                    }
+                }
+            }
+            ClickableText(
+                text = annotatedString,
+                onClick = { offset ->
+                    annotatedString.getStringAnnotations(
+                        tag = "clickable_text",
+                        start = offset,
+                        end = offset
+                    ).firstOrNull()?.let {
+                        onAction(RegisterAction.OnLoginClick)
+                    }
+                }
             )
         }
     }
